73e6d23fb5a4ad54103335f1a11ddfd5855c01fc
Cédric Maunier <cedric.maunier.lp4@gmail.com>
2020-01-16 14:49:32 +0100

    ajout des dossiers spring

---
+++ src/main/java/co/simplon/LesPromosSimplonApplication.java
@@ -0,0 +1,13 @@
+package co.simplon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesPromosSimplonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesPromosSimplonApplication.class, args);
+	}
+
+}

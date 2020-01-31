62803b779a347d3f3fd851e7b8890e1272e7353b
Cédric Maunier <cedric.maunier.lp4@gmail.com>
2020-01-31 09:17:22 +0100

    test findByPseudo repository

--- src/test/java/co/simplon/LesPromosSimplonApplicationTests.java
+++ src/test/java/co/simplon/LesPromosSimplonApplicationTests.java
@@ -1,13 +1,19 @@
 package co.simplon;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootTest
 class LesPromosSimplonApplicationTests {
 
 	@Test
 	void contextLoads() {
+		ConfigurableApplicationContext context = SpringApplication.run(LesPromosSimplonApplication.class);
+		assertTrue(context.isRunning());
+		context.close();
 	}
 
 }
